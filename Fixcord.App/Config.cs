a81e567fefc29dcfa911150a77c218d3ac936c18
C0ffeeCode <c0ffeecode@outlook.de>
2020-05-22 15:50:06 +0200

    .

--- Fixcord.App/Config.cs
+++ Fixcord.App/Config.cs
@@ -10,37 +10,37 @@ using System.Threading.Tasks;
 namespace Fixcord.App
 {
 	public static class Configuration
-    {
-        private static readonly string Path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Fixcord\\";
-        public static readonly string ConfigFile = Path + "config.json";
-        public static Config Config = new Config();
+	{
+		private static readonly string Path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Fixcord\\";
+		public static readonly string ConfigFile = Path + "config.json";
+		public static Config Config = new Config();
 
 		public static async void Save()
-        {
-            Directory.CreateDirectory(Path);
-            using (StreamWriter outputFile = new StreamWriter(ConfigFile))
-                await outputFile.WriteAsync(JsonSerializer.Serialize(Config));
-        }
+		{
+			Directory.CreateDirectory(Path);
+			using (StreamWriter outputFile = new StreamWriter(ConfigFile))
+				await outputFile.WriteAsync(JsonSerializer.Serialize(Config));
+		}
 
-        public static async Task Load()
-        {
-            try
-            {
-                StreamReader sr = new StreamReader(ConfigFile);
-                string file = await sr.ReadToEndAsync();
-                sr.Close();
+		public static async Task Load()
+		{
+			try
+			{
+				StreamReader sr = new StreamReader(ConfigFile);
+				string file = await sr.ReadToEndAsync();
+				sr.Close();
 
-                Config = JsonSerializer.Deserialize<Config>(file)!;
-            }
-            catch (FileNotFoundException ex)
-            {
-                Debug.WriteLine(ex.Message);
-            }
-        }
-    }
+				Config = JsonSerializer.Deserialize<Config>(file)!;
+			}
+			catch (FileNotFoundException ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
+		}
+	}
 
-    public class Config
-    {
-        public string? Token { get; set; }
-    }
+	public class Config
+	{
+		public string? Token { get; set; }
+	}
 }
