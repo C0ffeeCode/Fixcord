9800f3f6df082c7ae1fdc8824f3d2e7cf29a4b99
CoffeeCode <c0ffeecode@outlook.com>
2020-05-26 19:09:50 +0200

    :)

--- Fixcord.App/Controls/ChatControl.xaml.cs
+++ Fixcord.App/Controls/ChatControl.xaml.cs
@@ -16,59 +16,22 @@ namespace Fixcord.App.Controls
 		public ChatControl()
 		{
 			InitializeComponent();
-			//messagelisttest.ItemsSource = listabc;
-			ClientBot.client!.MessageReceived += (SocketMessage arg) =>
-			{
-				ClientBot.InvokeTextChannelChange();
-				return Task.CompletedTask;
-			};
-			ClientBot.SelectedTextChannelChanged += () => RefreshAsync();
+			ClientBot.client!.MessageReceived += (SocketMessage m) => Refresh();
+			ClientBot.SelectedTextChannelChanged += () => Refresh();
 		}
 
-		//return ClientBot.SelectedTextChannel == null ? (IEnumerable<IMessage>)new List<IMessage>() : ClientBot.SelectedTextChannel!.GetCachedMessages().AsEnumerable();
-
-		private void RefreshAsync()
-		{
-			// Im sorrry, 🍝
+		private Task Refresh()
+		{ // Im sorrry, 🍝
 			Dispatcher.Invoke(new Action(() =>
-			{
-				SocketTextChannel channel = ClientBot.SelectedTextChannel!;
-				if (channel == null)
-				{
-					return;
-				}
-
-				List<IMessage> listabc = new List<IMessage>();
-				try
-				{
-					var a = channel.GetMessagesAsync().ToListAsync().Result;
-					var b = a[1].AsEnumerable().OrderBy(s => s.Timestamp);
-
-					var x = channel.GetMessagesAsync().ToListAsync().Result;
-					//listabc = channel.GetMessagesAsync().ToEnumerable();
-
-					foreach (var i in x[1])
-					{
-						listabc.Add(i);
-						//var d = new ChatItem();
-						//d.Message = i;
-						////d.Width = this.Width;
-						//listabc.Add(d);
-					}
-					//Dispatcher.Invoke(
-					//	DispatcherPriority.DataBind,
-					//	new Action(() =>
-					messagelisttest.ItemsSource = listabc;
-				}
-				catch (Exception e)
-				{
-					Debug.WriteLine("Refreshing chat failed. " + e);
-				}
-				return;
-			}), DispatcherPriority.ContextIdle);
+					messagelisttest.ItemsSource =
+						(ClientBot.SelectedTextChannel!.GetMessagesAsync()
+							.ToListAsync().AsTask().Result)[1]
+							.AsEnumerable().OrderBy(s => s.Timestamp)
+			), DispatcherPriority.ContextIdle);
+			return Task.CompletedTask;
 		}
 
 		private void Button_Click(object sender, RoutedEventArgs e)
-			=> RefreshAsync();
+			=> Refresh();
 	}
 }
