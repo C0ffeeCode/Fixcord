0bb7b0b82d739f7f2efc8cd4b3f9604651feacff
CoffeeCode <c0ffeecode@outlook.com>
2020-06-02 20:54:21 +0200

    improved multi-window

--- Fixcord.Uwp/Pages/ChatPage.xaml.cs
+++ Fixcord.Uwp/Pages/ChatPage.xaml.cs
@@ -1,10 +1,16 @@
+using Discord.Rest;
 using Discord.WebSocket;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.System;
 using Windows.UI.Core;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Input;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -43,7 +49,7 @@ namespace Fixcord.Uwp.Pages
 				return;
 
 			await Dispatcher.RunAsync(CoreDispatcherPriority.High, () =>
-				messagelist.ItemsSource = channel.GetMessagesAsync()
+				messageList.ItemsSource = channel.GetMessagesAsync()
 					.ToListAsync().AsTask().Result[1]
 					.AsEnumerable().OrderBy(s => s.Timestamp)
 			);
@@ -61,6 +67,46 @@ namespace Fixcord.Uwp.Pages
 				channel.SendMessageAsync(messageInput.Text);
 				messageInput.Text = "";
 			}
+		}
+
+		private void messagelist_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			var isTransient = false;
+			FlyoutShowOptions myOption = new FlyoutShowOptions
+			{
+				ShowMode = isTransient ? FlyoutShowMode.Transient : FlyoutShowMode.Standard
+			};
+			CommandBarFlyout1.ShowAt(messageInput, myOption);
+		}
+
+		private void Copy_Click(object sender, RoutedEventArgs e)
+		{
+			var message = (RestUserMessage)messageList.SelectedItem;
+
+			var dataPackage = new DataPackage();
+			dataPackage.SetText(message.Content);
+			Clipboard.SetContent(dataPackage);
+
+			CommandBarFlyout1.Hide();
+		}
+
+		private async void Delete_Click(object sender, RoutedEventArgs e)
+		{
+			var message = messageList.SelectedItems;
+			foreach (RestUserMessage item in message)
+			{
+				try
+				{
+					await item.DeleteAsync();
+				}
+				catch (Exception ex)
+				{
+					Debug.WriteLine(ex);
+				}
+			}
+			await Refresh();
+
+			CommandBarFlyout1.Hide();
 		}
 	}
 }
--- Fixcord.Uwp/Pages/MainPage.xaml.cs
+++ Fixcord.Uwp/Pages/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using System;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -15,6 +17,13 @@ namespace Fixcord.Uwp.Pages
 		{
 			x.Initialize();
 			InitializeComponent();
+			
+			// Disable main-window internal Chat component by collapsing it
+			if (Configuration.multiWinModeEnabled)
+			{
+				abc.ColumnDefinitions[2].Width = new GridLength(0);
+				//abc.ColumnDefinitions[0].MaxWidth = double.PositiveInfinity;
+			}
 		}
 	}
 }
